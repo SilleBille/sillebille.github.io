24cf69526d1b0091eadd96cb2de7b36069c0ce3c
Dinesh Prasanth M K <mkdineshprasanth@gmail.com>
2019-03-30 23:24:22 -0400

    Adding custom portfolio style
    - CSS has been adjusted for custom section
    - TODO: Add new cards and auto-adjust
    
    Signed-off-by: Dinesh Prasanth M K <mkdineshprasanth@gmail.com>

--- js/mycustom.js
+++ js/mycustom.js
@@ -32,3 +32,16 @@ jQuery(document).ready(function($){
         });
     });
 });
+
+const cards = document.querySelectorAll('.card');
+
+function transition() {
+  if (this.classList.contains('active')) {
+    this.classList.remove('active')
+  } else {
+    this.classList.add('active');
+  }
+}
+
+cards.forEach(card => card.addEventListener('click', transition));
+ 
