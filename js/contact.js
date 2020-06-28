1c46af4fdf6be40e6cc3402362f2b62e488e89ea
Dinesh Prasanth M K <mkdineshprasanth@gmail.com>
2020-06-28 19:16:45 -0400

    Fix contact form
    Use Formspree to process contact form submissions
    
    Signed-off-by: Dinesh Prasanth M K <mkdineshprasanth@gmail.com>

--- js/contact.js
+++ js/contact.js
@@ -32,14 +32,14 @@ $(document).ready(function(){
             messages: {
                 name: {
                     required: "come on, you have a name, don't you?",
-                    minlength: "your name must consist of at least 2 characters"
+                    minlength: "your name must be longer than 2 character, right? ;)"
                 },
                 subject: {
                     required: "come on, you gotta tell me what you wanna talk about?",
-                    minlength: "your address must consist of at least 4 characters"
+                    minlength: "You sure that is a real subject?"
                 },
                 email: {
-                    required: "no email, no reply. Sorry! :D"
+                    required: "no email, no cookie. Sorry! :D"
                 },
                 message: {
                     required: "um...yea, you have to write something to send a message.",
@@ -80,3 +80,54 @@ $(document).ready(function(){
         
  })(jQuery)
 })
+
+
+/* Formspree AJAX email plugin */
+window.addEventListener("DOMContentLoaded", function() {
+
+    // get the form elements defined in your form HTML above
+
+    var form = document.getElementById("contactForm");
+    var button = document.getElementById("send-message-button");
+    var status = document.getElementById("contact-form-status");
+
+    var success_message = "Thanks for reaching out! I'll get back soon!!! :-)"
+    var failure_message = "Oops! There was a problem. Try again? :-("
+
+    // Success and Error functions for after the form is submitted
+
+    function success() {
+      form.reset();
+      button.style = "display: none ";
+      status.innerHTML = success_message;
+    }
+
+    function error() {
+      status.innerHTML = failure_message;
+    }
+
+    // handle the form submission event
+
+    form.addEventListener("submit", function(ev) {
+      ev.preventDefault();
+      var data = new FormData(form);
+      ajax(form.method, form.action, data, success, error);
+    });
+  });
+
+  // helper function for sending an AJAX request
+
+  function ajax(method, url, data, success, error) {
+    var xhr = new XMLHttpRequest();
+    xhr.open(method, url);
+    xhr.setRequestHeader("Accept", "application/json");
+    xhr.onreadystatechange = function() {
+      if (xhr.readyState !== XMLHttpRequest.DONE) return;
+      if (xhr.status === 200) {
+        success(xhr.response, xhr.responseType);
+      } else {
+        error(xhr.status, xhr.response, xhr.responseType);
+      }
+    };
+    xhr.send(data);
+}
